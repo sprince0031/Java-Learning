d0e8a501c53bd085c9b48ac97aa65524a268bc78
Siddharth Prince <siddharthprince31@gmail.com>
2020-01-14 17:30:33 +0530

    more practice using tests in different scenarios.

--- course/src/main/java/com/sprince0031/Person.java
+++ course/src/main/java/com/sprince0031/Person.java
@@ -1,12 +1,26 @@
 package com.sprince0031;
 
+import com.sprince0031.attributes.Name;
+
 public class Person {
 
+    public Person () {
+        // Explicitly defined default constructor for no real reason. 
+        
+    }
+    private String name = "Sprince0031";
+    private Name personName = new Name(name);
+
+    public String hello(String name) {
+        return "Hello " + name + "!";
+    }
+
+    public Name getName() {
+        return personName;
+    }
+    
     public String helloWorld() {
         return "Hello World!";
     }
 
-    // public static void main(String[] args) throws Exception {
-    //     System.out.println("Hello Java");
-    // }
 }
--- course/src/test/java/com/sprince0031/PersonTest.java
+++ course/src/test/java/com/sprince0031/PersonTest.java
@@ -2,6 +2,7 @@ package com.sprince0031;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import com.sprince0031.attributes.Name;
 
 public class PersonTest {
 
@@ -9,5 +10,18 @@ public class PersonTest {
     public void shouldReturnHelloWorld() {
         Person sprince0031 = new Person();
         assertEquals("Hello World!", sprince0031.helloWorld());
+        
+    }
+
+    @Test
+    public void shouldReturnNameObject() {
+        Person sprince0031 = new Person();
+        assertEquals(new Name("Sprince0031").name, sprince0031.getName().name);
+    }
+
+    @Test
+    public void shouldReturnHelloSprince0031() {
+        Person sprince0031 = new Person();
+        assertEquals("Hello Sprince0031!", sprince0031.hello("Sprince0031"));
     }
 }
